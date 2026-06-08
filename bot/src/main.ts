97b20a7bfd9a12a25e0c372f2ab6f28105e54273
Amigo20057 <denrizuk@gmail.com>
2026-06-08 13:35:49 +0300

    fix prefix

--- bot/src/main.ts
+++ bot/src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   app.enableCors();
 
   const configService = app.get(ConfigService);
